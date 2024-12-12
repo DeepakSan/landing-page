841f23050d8d511c1736754cbdeeaff2bf674b96
DeepakSan <sjanakisuresh@gmail.com>
2024-12-12 16:14:36 +0530

    Added all config file templpates and flask application factory

---
+++ src/landing_page/config.py
@@ -0,0 +1,9 @@
+import os
+
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_TRACK_MODIFICATIONS = os.environ.get('SQLALCHEMY_TRACK_MODIFICATIONS')
+    SQLALCHEMY_ECHO = os.environ.get('SQLALCHEMY_ECHO')
+
